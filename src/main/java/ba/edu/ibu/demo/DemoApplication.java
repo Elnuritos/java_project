cef0589e0d46958bd52468b2420fd19df711e9b5
elnuritos <elnu1990@list.ru>
2023-10-05 10:49:32 +0200

    init

---
+++ src/main/java/ba/edu/ibu/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/edu/ibu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
